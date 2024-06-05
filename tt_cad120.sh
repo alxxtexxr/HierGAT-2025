5711c473be435f65a29e4bd8a64e957e96afdf48
wjx1198 <41059906+wjx1198@users.noreply.github.com>
2024-06-05 16:08:04 +0800

    Update tt_cad120.sh

--- tt_cad120.sh
+++ tt_cad120.sh
@@ -1,7 +1,7 @@
-CUDA_VISIBLE_DEVICES=1 nohup python -u train_cad120_1.py > outputs_cuantrans/cad120/cad120_1_stage2_0.3.txt &
+CUDA_VISIBLE_DEVICES=1 nohup python -u train_cad120_1.py > outputs_cuantrans/cad120/cad120_1_stage1.txt &
 
-CUDA_VISIBLE_DEVICES=3 nohup python -u train_cad120_3.py > outputs_cuantrans/cad120/cad120_3_stage2_0.3.txt &
+CUDA_VISIBLE_DEVICES=3 nohup python -u train_cad120_3.py > outputs_cuantrans/cad120/cad120_3_stage1.txt &
 
-CUDA_VISIBLE_DEVICES=2 nohup python -u train_cad120_4.py > outputs_cuantrans/cad120/cad120_4_stage2_0.3.txt &
+CUDA_VISIBLE_DEVICES=2 nohup python -u train_cad120_4.py > outputs_cuantrans/cad120/cad120_4_stage1.txt &
 
-CUDA_VISIBLE_DEVICES=0 nohup python -u train_cad120_5.py > outputs_cuantrans/cad120/cad120_5_stage2_0.3.txt &
+CUDA_VISIBLE_DEVICES=0 nohup python -u train_cad120_5.py > outputs_cuantrans/cad120/cad120_5_stage1.txt &
